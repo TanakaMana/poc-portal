032b3a7f936547708a99b508a5ebe15146a08d13
TanakaMana <tnmn@TanakaMananoMacBook-Pro.local>
2019-05-04 10:45:14 +0900

    [create] project update function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,6 +20,19 @@ class ProjectsController < ApplicationController
   	@project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to root_path, notice: '実証実験事例の更新に成功しました'
+    else
+      redirect_to edit_project_path, alert: '実証実験事例の更新に失敗しました'
+    end
+  end
+
   private
 
   def project_params
